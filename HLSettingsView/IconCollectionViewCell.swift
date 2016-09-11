1073b9131d462a82b4c18a3bc9afa6c8f4332b14
Hanjie Liu <Hollis@Hanjies-MacBook-Pro.local>
2016-09-11 01:11:12 -0400

    Embedded View

--- HLSettingsView/IconCollectionViewCell.swift
+++ HLSettingsView/IconCollectionViewCell.swift
@@ -10,4 +10,19 @@ import UIKit
 
 class IconCollectionViewCell: UICollectionViewCell {
     
+
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var iconImage: UIImageView!
+    
+    var name = ""
+    var imageName = ""{didSet{
+        setup()
+        }}
+    
+    func setup(){
+        title.text = name
+        iconImage.image = UIImage(named: imageName)
+    }
+    
 }
--- HLSettingsView/SpringboardLayout.swift
+++ HLSettingsView/SpringboardLayout.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 class SpringboardLayout: UICollectionViewFlowLayout {
-
+    
+    override init() {
+        super.init()
+        
+        self.scrollDirection = .horizontal
+        
+        self.itemSize = CGSize(width: 60, height: 80)
+        self.minimumInteritemSpacing = 5
+        self.minimumLineSpacing = 20
+        self.sectionInset = UIEdgeInsetsMake(5,32,5,32)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
