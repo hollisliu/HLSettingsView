2d4b504e8e2b9c22c004e906d2346ff76655707a
Hanjie Liu <Hollis@Hanjies-MacBook-Pro.local>
2016-09-10 23:59:13 -0400

    collectionView

---
+++ HLSettingsView/IconCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  IconCollectionViewCell.swift
+//  HLSettingsView
+//
+//  Created by Hanjie Liu on 9/10/16.
+//  Copyright © 2016 Hanjie Liu. All rights reserved.
+//
+
+import UIKit
+
+class IconCollectionViewCell: UICollectionViewCell {
+    
+}
